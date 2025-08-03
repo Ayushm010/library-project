96e919ab796cdfccaf052f0a0d661ac702ee0fe1
Ayushm010 <mohapatraayush587@gmail.com>
2025-08-03 18:19:29 +0530

    feat: add basic CSS layout and cancel button

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const card = document.querySelector(".card");
+const btn = document.querySelector("#btn");
+const inputBox = document.getElementById("input-box");
+const form = document.getElementById("bookForm");
+const closeBtn = document.getElementById("close");
 
 function Book(name, author, pages, status, uId) {
     this.name = name;
@@ -8,9 +12,7 @@ function Book(name, author, pages, status, uId) {
     this.uId = uId;
 
 }
-Book.prototype.disp = function () {
-    console.log(`${this.name} by ${this.author}, ${this.pages} pages long, ${this.status}`);
-}
+
 
 const book1 = new Book("1984", "George Orwell", 328, "Read", 1);
 const book2 = new Book("To Kill a Mockingbird", "Harper Lee", 281, "Unread", 2);
@@ -24,26 +26,10 @@ function addBookToLibrary(name, author, pages, status) {
     let uId = crypto.randomUUID();
 
     let book = new Book(name, author, pages, status, uId);
-    book.disp();
 
-    myLibrary.push(book);
-    const newDiv = document.createElement("div");
-    newDiv.style.backgroundColor = "white";
-    newDiv.style.color = "#060606ff";
-    newDiv.style.padding = "20px";
-    newDiv.style.border = "2px solid gray"
-    newDiv.style.borderRadius = "10px";
-    newDiv.style.margin = "10px 0";
-    newDiv.style.fontSize = "18px";
-    newDiv.style.fontFamily = "Arial, sans-serif";
+    myLibrary.push(book); // Adding new book to the array
 
-    newDiv.innerHTML = `
-                        ${myLibrary[myLibrary.length - 1].name}<br>
-                        ${myLibrary[myLibrary.length - 1].author}<br>
-                        ${myLibrary[myLibrary.length - 1].pages}<br>
-                        ${myLibrary[myLibrary.length - 1].status}`;
-
-    card.appendChild(newDiv);
+    addToPage(myLibrary.length-1);// Adds the new book to the page
 
 }
 console.log(myLibrary[0]);
@@ -51,7 +37,13 @@ console.log(myLibrary[0]);
 
 function showBooks() {
     for (let i = 0; i < myLibrary.length; i++) {
-        let newDiv = document.createElement("div");
+       addToPage(i);
+    }
+
+}
+
+function addToPage(i){
+     const newDiv = document.createElement("div");
         newDiv.style.backgroundColor = "white";
         newDiv.style.color = "#060606ff";
         newDiv.style.border = "2px solid gray"
@@ -68,14 +60,8 @@ function showBooks() {
                         ${myLibrary[i].status}`;
 
         card.appendChild(newDiv);
-    }
-
 }
-showBooks();
-const btn = document.querySelector("#btn");
-const inputBox = document.getElementById("input-box");
-const form = document.getElementById("bookForm");
-const closeBtn = document.getElementById("close")
+
 
 
 btn.addEventListener("click", () => {
@@ -97,6 +83,8 @@ form.addEventListener("submit", (e) => {
 });
 
 
-document.getElementById("cancelBtn").addEventListener("click", () => {
+document.getElementById("close").addEventListener("click", () => {
     document.getElementById("input-box").close();
 });
+
+showBooks();
